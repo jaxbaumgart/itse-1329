b79d39bff2ce717782c8a94d484b2163e1d5476e
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-02-07 05:01:41 -0600

    Draft

--- week02/compoundInterest.kt
+++ week02/compoundInterest.kt
@@ -1,19 +1,10 @@
 fun main() {
     print("Enter principal interest amount >> ")
-    val principal = readln().toInt()
+    val principal = readln().toDouble()
     print("Enter annual interest rate >> ")
     val interest = readln().toDouble()
     print("Enter compound frequency >> ")
     val frequency = readln().toInt()
     print("Enter number of years >> ")
     val years = readln().toInt()
-    var interestTotal = (1 + interest / frequency)
-    println(interestTotal)
-    val loop = frequency * years
-    for (i in 1..loop) {
-        interestTotal *= interestTotal
-    }
-    println(interestTotal)
-    val final = principal * interestTotal
-    print("The final amount is $$final.")
 }
