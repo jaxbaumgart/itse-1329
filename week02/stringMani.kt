3cbc336b41b47962b61dfa553d3aaea46afff5f6
jabaumgart <jacksonbaumgart11@gmail.com>
2023-02-07 04:21:49 -0600

    Final

--- week02/stringMani.kt
+++ week02/stringMani.kt
@@ -1,3 +1,12 @@
-main() {
-  
+fun main() {
+    print("Enter the name of your favorite city >> ")
+    val city = readln()
+    val cityLength = city.length
+    println("$city is $cityLength letters long.")
+    val cityUpper = city.uppercase()
+    println(cityUpper)
+    val cityLower = city.lowercase()
+    println(cityLower)
+    val cityFirst = city[0]
+    println(cityFirst)
 }
