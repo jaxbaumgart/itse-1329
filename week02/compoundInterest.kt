411653b8bc9bd7b70fe926b32737aa92a610dbdc
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-02-09 02:46:22 -0600

    Final compoundInterest.kt

--- week02/compoundInterest.kt
+++ week02/compoundInterest.kt
@@ -1,10 +1,15 @@
+import kotlin.math.*
+
 fun main() {
     print("Enter principal interest amount >> ")
-    val principal = readln().toDouble()
-    print("Enter annual interest rate >> ")
-    val interest = readln().toDouble()
+    val p = readln().toDouble()
+    print("Enter annual interest rate (in decimal form) >> ")
+    val r = readln().toDouble()
     print("Enter compound frequency >> ")
-    val frequency = readln().toInt()
+    val n = readln().toDouble()
     print("Enter number of years >> ")
-    val years = readln().toInt()
+    val t = readln().toInt()
+    var A = p * (1 + r / n).pow(n * t.toDouble())
+    A = (A * 100.0).roundToInt() / 100.0
+    print("The amount of money in the account after $t years is $$A.")
 }
