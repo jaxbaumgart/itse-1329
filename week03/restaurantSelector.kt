e5d4c1d5ab6e0c5cd62c95a1cc0dfae520d0dee1
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-02-27 20:27:45 -0800

    Final restaurantSelector.kt

---
+++ week03/restaurantSelector.kt
@@ -0,0 +1,38 @@
+package week03
+
+fun main() {
+    print("Is anyone in your party vegetarian? >> ")
+    val vegetarian = readln().lowercase()
+    print("Is anyone in your party vegan? >> ")
+    val vegan = readln().lowercase()
+    print("Is anyone in your party gluten-free? >> ")
+    val gluten = readln().lowercase()
+    var rest1 = 1
+    var rest2 = 1
+    var rest3 = 1
+    if (vegetarian == "yes") {
+        rest1 = 0
+    }
+    if (vegan == "yes") {
+        rest1 = 0
+        rest2 = 0
+        rest3 = 0
+    }
+    if (gluten == "yes") {
+        rest1 = 0
+        rest3 = 0
+    }
+    println("Here are your restaurant choices:")
+    println("Corner Cafe")
+    println("The Chef's Kitchen")
+    if (rest1 == 1) {
+        print("Joe's Gourmet Burgers")
+    }
+    if (rest2 == 1) {
+        print("Main Street Pizza Company")
+    }
+    if (rest3 == 1) {
+        print("Mama's Fine Italian")
+    }
+
+}
