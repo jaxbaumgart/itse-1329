612f52b9e173c23a357591bde3503dd01c79f622
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-02-27 20:28:13 -0800

    Draft runRace.kt

---
+++ week03/runRace.kt
@@ -0,0 +1,19 @@
+package week03
+
+fun main() {
+    print("Enter name of competitor 1 >> ")
+    val name1 = readln()
+    print("Enter race time of competitor 1 >> ")
+    val time1 = readln().toDouble()
+    print("Enter name of competitor 2 >> ")
+    val name2 = readln()
+    print("Enter race time of competitor 2 >> ")
+    val time2 = readln().toDouble()
+    print("Enter name of competitor 3 >> ")
+    val name3 = readln()
+    print("Enter race time of competitor 3 >> ")
+    val time3 = readln().toDouble()
+    var first = 0
+    var second = 0
+    var third = 0
+}
