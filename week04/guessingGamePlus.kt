408da6885e23191841c5eea561707c06620bbd24
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-03-08 02:58:44 -0600

    Create week04 files.

---
+++ week04/guessingGamePlus.kt
@@ -0,0 +1,10 @@
+package week04
+
+/*
+Enhance the program that you wrote for the previous assignment so it keeps a count of the number of guesses that the user
+makes. When the user correctly guesses the random number, the program should display the number of guesses.
+*/
+
+fun main() {
+
+}
---
+++ week04/letterCounter.kt
@@ -0,0 +1,10 @@
+package week04
+
+/*
+Write a program that asks the user to enter a string, and then asks the user to enter a character.
+The program should count and display the number of times that the specified character appears in the string.
+*/
+
+fun main() {
+
+}
