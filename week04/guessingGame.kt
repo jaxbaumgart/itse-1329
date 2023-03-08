0bbf0b5b5ad62b81601715f47827423e566e8540
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-03-08 03:33:43 -0600

    Final guessingGame.kt

--- week04/guessingGame.kt
+++ week04/guessingGame.kt
@@ -1,12 +1,5 @@
 package week04
 
-/*
-Write a program that generates a random number and asks the user to guess what the number is.
-If the user’s guess is higher than the random number, the program should display “Too high, try again.”
-If the user’s guess is lower than the random number, the program should display “Too low, try again.”
-The program should use a loop that repeats until the user correctly guesses the random number.
-*/
-
 fun main() {
     val number =(0..1001).random()
     print("Guess a number >> ")
