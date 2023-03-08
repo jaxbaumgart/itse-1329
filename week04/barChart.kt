96abd79d1d316c531c3b88b96fbc403542e5339c
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-03-08 03:19:51 -0600

    Final barChart.kt

--- week04/barChart.kt
+++ week04/barChart.kt
@@ -1,25 +1,41 @@
 package week04
 
-/*
-Write a program that asks the user to enter today’s sales for five stores.
-The program should display a bar chart comparing each store’s sales.
-Create each bar in the bar chart by displaying a row of asterisks.
-Each asterisk should represent $100 of sales.
-Here is an example of the program’s output:
-Enter today's sales for store 1: 1000 [Enter]
-Enter today's sales for store 2: 1200 [Enter]
-Enter today's sales for store 3: 1800 [Enter]
-Enter today's sales for store 4: 800 [Enter]
-Enter today's sales for store 5: 1900 [Enter]
-SALES BAR CHART
-Store 1: **********
-Store 2: ************
-Store 3: ******************
-Store 4: ********
-Store 5: *******************
-Write your code in a file named barChart.kt in your unit04 folder. Turn in the GitHub link for a grade.
-*/
-
 fun main() {
+    print("Enter today's sales for store 1 >> ")
+    val store1 = readln().toInt()
+    print("Enter today's sales for store 2 >> ")
+    val store2 = readln().toInt()
+    print("Enter today's sales for store 3 >> ")
+    val store3 = readln().toInt()
+    print("Enter today's sales for store 4 >> ")
+    val store4 = readln().toInt()
+    print("Enter today's sales for store 5 >> ")
+    val store5 = readln().toInt()
 
+    println()
+    println("SALES BAR CHART")
+    print("Store 1 >> ")
+    repeat(store1 / 100) {
+        print("*")
+    }
+    println()
+    print("Store 2 >> ")
+    repeat(store2 / 100) {
+        print("*")
+    }
+    println()
+    print("Store 3 >> ")
+    repeat(store3 / 100) {
+        print("*")
+    }
+    println()
+    print("Store 4 >> ")
+    repeat(store4 / 100) {
+        print("*")
+    }
+    println()
+    print("Store 5 >> ")
+    repeat(store5 / 100) {
+        print("*")
+    }
 }
