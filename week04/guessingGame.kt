e8af4fb91b34a81bbfa5d8a881979ab6bfb6f76b
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-03-08 03:33:20 -0600

    Final guessingGame.kt

--- week04/guessingGame.kt
+++ week04/guessingGame.kt
@@ -8,5 +8,18 @@ The program should use a loop that repeats until the user correctly guesses the 
 */
 
 fun main() {
-
+    val number =(0..1001).random()
+    print("Guess a number >> ")
+    var guess = readln().toInt()
+    while (guess != number) {
+        if (guess < number) {
+            println("Too low, try again.")
+        }
+        if (guess > number) {
+            println("Too high, try again.")
+        }
+        print("Guess a number >> ")
+        guess = readln().toInt()
+    }
+    print("Congrats! You guessed the number! The number was $number.")
 }
