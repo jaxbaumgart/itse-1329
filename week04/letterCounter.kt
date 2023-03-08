edd683eb6616f124184c312b9d8932a7a8bd94c9
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-03-08 08:50:06 -0600

    Final letterCounter.kt

--- week04/letterCounter.kt
+++ week04/letterCounter.kt
@@ -1,10 +1,17 @@
 package week04
-
-/*
-Write a program that asks the user to enter a string, and then asks the user to enter a character.
-The program should count and display the number of times that the specified character appears in the string.
-*/
+import java.util.Scanner
 
 fun main() {
-
+    val scanner = Scanner(System.`in`)
+    print("Enter a word >> ")
+    val word = readln().lowercase()
+    print("Enter a character >> ")
+    val char = scanner.next().single().lowercaseChar()
+    var counter = 0
+    for (i in 0..word.length - 1) {
+        if (word[i] == char) {
+            counter++
+        }
+    }
+    print("$char appears in $word $counter times.")
 }
