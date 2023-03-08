1bcc7279f974f2607e6e4120643a10a1e2b5f5e0
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-03-08 09:50:22 -0500

    Final population.kt

--- week04/population.kt
+++ week04/population.kt
@@ -6,12 +6,33 @@ The program should ask for the starting number of organisms, their average daily
 and the number of days they will multiply. For example, a population might begin with two organisms,
 have an average daily increase of 50 percent, and will be allowed to multiply for seven days.
 The program should use a loop to display the size of the population for each day.
-
-Input Validation: Do not accept a number less than 2 for the starting size of the population.
-Do not accept a negative number for the average daily population increase.
-Do not accept a number less than 1 for the number of days they will multiply.
 */
 
 fun main() {
-
+    print("Enter starting number of organisms >> ")
+    var population = readln().toDouble()
+    while (population < 2) {
+        print("Starting population must be 2 or greater >> ")
+        population = readln().toDouble()
+    }
+    print("Enter average daily population increase (as a percentage) >> ")
+    var incr = readln().toDouble() / 100
+    while (incr < 0) {
+        print("Daily population increase must be positive >> ")
+        incr = readln().toDouble() / 100
+    }
+    print("Enter number of days to multiply >> ")
+    var days = readln().toInt()
+    while (days < 1) {
+        print("Days to multiply must be 1 or greater >> ")
+        days = readln().toInt()
+    }
+    println()
+    println("Population after day 0 is $population.")
+    for (i in 1..days) {
+        population *= (1 + incr)
+        println("Population after day $i is $population.")
+    }
+    println()
+    print("After $days days, the population is $population.")
 }
