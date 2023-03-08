70ceab98b6700362110169d61386b360b8eb5bcc
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-03-08 03:34:48 -0600

    Final guessingGamePlus.kt

--- week04/guessingGamePlus.kt
+++ week04/guessingGamePlus.kt
@@ -1,10 +1,20 @@
 package week04
 
-/*
-Enhance the program that you wrote for the previous assignment so it keeps a count of the number of guesses that the user
-makes. When the user correctly guesses the random number, the program should display the number of guesses.
-*/
-
 fun main() {
-
+    val number =(0..1001).random()
+    var tries = 1
+    print("Guess a number >> ")
+    var guess = readln().toInt()
+    while (guess != number) {
+        if (guess < number) {
+            println("Too low, try again.")
+        }
+        if (guess > number) {
+            println("Too high, try again.")
+        }
+        tries++
+        print("Guess a number >> ")
+        guess = readln().toInt()
+    }
+    print("Congrats! You guessed the number in $tries tries! The number was $number.")
 }
