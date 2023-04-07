2d53a621ec245ec43ba10c1c96d988d1665a3230
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-04-07 18:32:19 -0500

    Final priceCalculator.kt

--- week05/priceCalculator.kt
+++ week05/priceCalculator.kt
@@ -1,5 +1,10 @@
 package week05
 
 fun main() {
+    println(calculateRetail(5.00, 100.0))
+    println(calculateRetail(5.00, 50.0))
+}
 
+fun calculateRetail(cost: Double, markup: Double): Double {
+    return cost * (1 + (markup / 100))
 }
