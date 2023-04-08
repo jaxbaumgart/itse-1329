a719573d3b1de9fadaef926cda7f05f3b4fdb3cc
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-04-08 05:08:09 -0500

    Final celsiusTable.kt.kt

--- week05/celsiusTable.kt
+++ week05/celsiusTable.kt
@@ -1,5 +1,13 @@
 package week05
 
 fun main() {
+    println("Fahrenheit\tCelsius")
+    for (num in 1..20) {
+        val celsius = celsius(num.toDouble())
+        println("$num\t\t\t$celsius")
+    }
+}
 
+fun celsius(fahrenheit: Double): Double {
+    return 5.0/9.0 * (fahrenheit - 32.0)
 }
