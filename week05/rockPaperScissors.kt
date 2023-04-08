da431eaa61bb2c9e3221068f9d4a00dd5a2bab1b
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-04-08 05:42:22 -0500

    Draft/incomplete rockPaperScissors.kt

--- week05/rockPaperScissors.kt
+++ week05/rockPaperScissors.kt
@@ -1,5 +1,27 @@
 package week05
 
 fun main() {
+    println(startGame())
+    randomNumber()
+    userTurn()
+}
 
+fun randomNumber(): Int {
+    return (1..3).random()
+}
+
+fun userTurn(): Int {
+    println("Enter a choice >>")
+    val choice = readln().lowercase()
+    if (choice == "rock") {
+        return 1
+    } else if (choice == "paper") {
+        return 2
+    } else {
+        return 3
+    }
+}
+
+fun startGame(): String {
+    return "This is Rock Paper Scissors"
 }
