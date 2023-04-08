cdbb6583defb743fa12dffed3c52290c2574e065
jaxbaumgart <60806269+jaxbaumgart@users.noreply.github.com>
2023-04-08 04:56:31 -0500

    Final fallingDistance.kt

--- week05/fallingDistance.kt
+++ week05/fallingDistance.kt
@@ -1,5 +1,11 @@
 package week05
 
 fun main() {
+    for (i in 1..10) {
+        println(fallingDistance(i))
+    }
+}
 
+fun fallingDistance(seconds: Int): Double {
+    return 0.5 * (9.8 * (seconds * seconds))
 }
